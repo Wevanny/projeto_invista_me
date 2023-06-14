49a5aa4bdce943b9e2ba341ba2edc52321e2b852
Wevanny Gomes <wdesgomes.ti@gmail.com>
2023-06-14 16:07:28 -0300

    ocultação de secret_key

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from pathlib import Path
 
@@ -20,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w0ws*_3i0eh5li602)#wuk*4s$%l$nt$7-xkxyh!74%$k$93*f'
+#SECRET_KEY = 'django-insecure-w0ws*_3i0eh5li602)#wuk*4s$%l$nt$7-xkxyh!74%$k$93*f'
+import os
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
